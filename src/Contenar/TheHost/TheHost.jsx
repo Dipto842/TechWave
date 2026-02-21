a3dc970fb015545096cbaf4e340cee98da4f75ee
Dipto842 <diptobakshi72@gmail.com>
2026-02-22 02:45:18 +0600

    aa

--- src/Contenar/TheHost/TheHost.jsx
+++ src/Contenar/TheHost/TheHost.jsx
@@ -11,15 +11,15 @@ const TheHost = () => {
  
   <div class="card-body d-flex align-items-center gap-4">
    <div className='p-4'>
-    <img src="/Vector (1).png" alt="" />
+    <img src="/Vector(1).png" alt="" />
    </div>
 
    <div className=' text-start'>
     <h1 className='hedar1'>Alex Kumar</h1>
     <p className='peragirap'>Tech entrepreneur, productivity expert, and lifelong learner. Alex has been in the tech industry for over 10 years, working with startups and Fortune 500 companies. Through TechWave, Alex shares insights from industry leaders and practical advice for anyone looking to grow in tech.</p>
     <div>
-        <img src="/Group 10.png" alt="" />
-        <img src="/Group 11.png" alt="" />
+        <img src="/Group-10.png" alt="" />
+        <img src="/Group-11.png" alt="" />
         <img src="/Group 12.png" alt="" />
         <img src="/Group 13.png" alt="" />
     </div>
