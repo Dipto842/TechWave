fa3bcacc0e8170106211eef74ad92178cc68b09e
Dipto842 <diptobakshi72@gmail.com>
2026-02-22 03:02:17 +0600

    aaa

--- src/Contenar/WhyTechWave/WhyTechWave.jsx
+++ src/Contenar/WhyTechWave/WhyTechWave.jsx
@@ -10,7 +10,7 @@ const WhyTechWave = () => {
     <div className="col-md-12 col-lg-4 ">
         <div className="card h-100 rounded-4 p-4 cardColor text-light " >
   <div class="card-body">
-    <div><img className='pending' src="  /Hedphon.png" alt="" /></div>
+    <div><img className='pending11' src="/Hedphon.png" alt="" /></div>
     <h5 class="card-title pt-4 title">Premium Audio Quality</h5>
     
     <p class="card-text text-style">Experience crystal-clear sound with professional-grade recording equipment and expert audio engineering. Every episode is mastered for optimal listening across all devices and platforms.</p>
@@ -26,7 +26,7 @@ const WhyTechWave = () => {
     <div className="col-md-12 col-lg-8 ">
         <div className="card rounded-4 mb-4 p-4 cardColor text-light" >
   <div class="card-body">
-    <div><img className=' pending' src="  /Phon.png" alt="" /></div>
+    <div><img className=' pending11' src="/Phon.png" alt="" /></div>
     <h5 class="card-title pt-4 title">Mobile Friendly</h5>
     
     <p class="card-text text-style">Listen on the go with seamless mobile experience and offline downloads. the bulk of the card’s content.</p>
@@ -35,7 +35,7 @@ const WhyTechWave = () => {
 </div>
         <div className="card rounded-4 p-4 cardColor text-light" >
   <div class="card-body ">
-    <div><img className='pending' src="  /Locechon.png" alt="" /></div>
+    <div><img className='pending11' src="/Locechon.png" alt="" /></div>
     <h5 class="card-title pt-4 title">Global Community</h5>
     
     <p class="card-text text-style">Connect with tech enthusiasts from over 120 countries worldwide.</p>
@@ -57,7 +57,7 @@ const WhyTechWave = () => {
        <div className="col-md-12 col-lg-8  ">
         <div className="card rounded-4 p-4 cardColor text-light " >
   <div class="card-body">
-    <div><img className='pending' src="  /interviews.png" alt="" /></div>
+    <div><img className='pending1' src="/interviews.png" alt="" /></div>
     <h5 class="card-title pt-4 title">Exclusive Interviews</h5>
     
     <p class="card-text text-style">Get unprecedented access to industry titans, startup founders, and tech visionaries. Our host secures exclusive conversations you won't find anywhere else, diving deep into their journeys, failures, and success strategies.</p>
@@ -69,7 +69,7 @@ const WhyTechWave = () => {
     <div className="col-md-12 col-lg-4 hight">
         <div className="card rounded-4 p-4 cardColor text-light" >
   <div class="card-body">
-    <div><img className='pending' src="  /Book.png" alt="" /></div>
+    <div><img className='pending1' src="/Book.png" alt="" /></div>
     <h5 class="card-title pt-4 title">Rich Resources</h5>
     <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
     <p class="card-text text-style">Access show notes, transcripts, and additional learning materials for every episode.</p>
